e4220128a8a662869944b96c6a165d6f1e88af52
Fernando Pineda <fernandopinedamgc@gmail.com>
2025-11-15 13:21:17 -0600

    sorteo aab

--- sorteo/index.php
+++ sorteo/index.php
@@ -7,7 +7,7 @@ require_once 'config/database.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sorteo Mizton - Participa y Gana</title>
+    <title>Sorteo Pahuata - Participa y Gana</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="assets/css/sorteo.css" rel="stylesheet">
